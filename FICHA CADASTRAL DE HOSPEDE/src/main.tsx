cb60d838fc5d6c468a5d41ea6ec331216e6cf77b
Seu Nome <seu_email_do_github@gmail.com>
2026-02-13 11:56:54 -0300

    security_ativando_blindagem_anti_inspecao_nivel_9

--- FICHA CADASTRAL DE HOSPEDE/src/main.tsx
+++ FICHA CADASTRAL DE HOSPEDE/src/main.tsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// SILÊNCIO DE PRODUÇÃO
+if (import.meta.env.PROD) {
+  console.log = () => {};
+  console.debug = () => {};
+  console.info = () => {};
+  console.warn = () => {};
+  console.error = () => {};
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
